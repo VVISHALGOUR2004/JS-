9027a97613c2309447b0022bfb9ec5a461f384a7
Vishal Gour <vishalgour624@gmail.com>
2025-06-24 09:55:30 +0000

    Js Pratice

---
+++ script.js
@@ -0,0 +1,64 @@
+// conditional stat 
+
+// let age = 15 ;
+
+// if (age<18) {
+//     console.log("you cannot give vote");
+
+// }else if(age>=18){
+//     console.log("You can give vote");
+
+// }
+
+
+// ternary condition 
+
+// const age = 20
+
+
+// age >=16 ? console.log("Yes") : console.log("nope");
+
+
+// switch cases
+
+// let names_list = 2
+
+// switch (names_list) {
+//     case 1:
+//         console.log("hello ");
+
+//         break;
+//    case 2:
+//         console.log("Vishal ");
+
+//         break;
+//    case 3:
+//         console.log("Gour ");
+
+//         break;
+
+//     default:
+//         console.log("Invalid option");
+
+//         break;
+// }
+
+
+let a = "+"
+let b = 20
+let c = 40
+
+switch (a) {
+    case "+":
+        console.log(c + b);
+        break;
+    case "-":
+        console.log(c - b);
+        break;
+    case "+":
+        console.log(a * b);
+        break;
+
+    default:
+        break;
+}
